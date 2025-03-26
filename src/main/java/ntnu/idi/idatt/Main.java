caa7fcdd435ee093807853f203bdf2bce6771523
oscarkamartins <oscarkamartins@gmail.com>
2025-03-26 16:49:45 +0100

    finished setting up javafx and everything ui related

--- src/main/java/ntnu/idi/idatt/Main.java
+++ src/main/java/ntnu/idi/idatt/Main.java
@@ -1,7 +1,23 @@
 package ntnu.idi.idatt;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ntnu.idi.idatt.UI.scenes.StartPageScene;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage stage) {
+    Router.setStage(stage);
+
+    // Replace with your actual first scene
+    Scene root = new StartPageScene();
+    Router.navigateTo(root);
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
