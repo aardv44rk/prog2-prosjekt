1e96e071e7da4a78affa4ec0e50ca0616f92398c
Audun Aakermann <audunaakermann@gmail.com>
2025-03-26 14:02:21 +0100

    Add LadderAction

--- src/main/java/ntnu/idi/idatt/models/LadderAction.java
+++ src/main/java/ntnu/idi/idatt/models/LadderAction.java
@@ -1,13 +1,23 @@
 package ntnu.idi.idatt.models;
 
 public class LadderAction implements TileAction {
-  private int destionationTileId;
+  private int destinationTileId;
+  private String description;
 
   public LadderAction(int destinationTileId, String description) {
-    this.destionationTileId = destinationTileId;
+    this.destinationTileId = destinationTileId;
+    this.description = description;
   }
   
   @Override
   public void perform(Player player) {
+    System.out.println(description);
+
+    Board board = player.getGame().getBoard();
+
+    Tile destinationTile = board.getTile(destinationTileId);
+    player.placeOnTile(destinationTile);
+
+    System.out.println(player.getName() + " moved to tile " + destinationTileId);
   }
 }
