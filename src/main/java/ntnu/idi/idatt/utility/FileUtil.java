13fbe9ed0fd5c1eab541912e7d35a42f7053ec37
Audun Aakermann <audunaakermann@gmail.com>
2025-04-18 21:40:03 +0900

    Add prototype methods for FileUtil: - Read file - Write file - Append a string to file - Create dirs

--- src/main/java/ntnu/idi/idatt/utility/FileUtil.java
+++ src/main/java/ntnu/idi/idatt/utility/FileUtil.java
@@ -1,4 +1,36 @@
 package ntnu.idi.idatt.utility;
 
+import java.util.List;
+
+/**
+ * Utility class for reading and writing files.
+ */
 public class FileUtil {
+  public static String readString(String filePath) {
+    return null;
+  }
+
+  public static List<String> readLines(String filePath) {
+    return null;
+  }
+
+  public static void writeString(String filePath, String content) {
+
+  }
+
+  public static void writeLines(String filePath, List<String> lines) {
+
+  }
+
+  public static void appendString(String filePath, String content) {
+
+  }
+
+  public static void createDirectories(String dirPath) {
+
+  }
+
+  public static boolean fileExists(String filePath) {
+    return false;
+  }
 }
