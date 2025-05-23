1006318a3a907556e0aa44c5e9900ff94653e49c
oscarkamartins <oscarkamartins@gmail.com>
2025-05-23 02:37:58 +0200

    javafx barchart 1, oscar 0

--- src/main/java/ntnu/idi/idatt/components/MoneyGraph.java
+++ src/main/java/ntnu/idi/idatt/components/MoneyGraph.java
@@ -17,31 +17,36 @@ import ntnu.idi.idatt.utility.StyleUtil;
  */
 public class MoneyGraph extends BarChart<String, Number> {
 
+  private static final double TARGET_BAR_WIDTH = 30.0;
+  private static final double COMPONENT_TOTAL_WIDTH = 300.0;
+  private static final double MAGIC_NUMBER = 140.0;
+
   private final Map<String, XYChart.Data<String, Number>> dataMap = new HashMap<>();
 
   /**
    * Constructor for the MoneyGraph component.
    *
-   * @param player A list of pairs containing player names and their corresponding colors.
+   * @param players A list of pairs containing player names and their corresponding colors.
    */
-  public MoneyGraph(List<Pair<String, Color>> player) {
+  public MoneyGraph(List<Pair<String, Color>> players) {
     super(new CategoryAxis(), new NumberAxis());
 
     getStyleClass().add("money-graph");
 
-    setCategoryGap(50);
-
+  
     NumberAxis yAxis = (NumberAxis) getYAxis();
     yAxis.getStyleClass().add("money-graph-yaxis");
     yAxis.setAutoRanging(false);
     yAxis.setLowerBound(0);
-    yAxis.setUpperBound(1100);
+    yAxis.setUpperBound(1150);
 
     CategoryAxis xAxis = (CategoryAxis) getXAxis();
     xAxis.getStyleClass().add("money-graph-xaxis");
+    xAxis.setStartMargin(0.0);
+    xAxis.setEndMargin(0.0);
 
     Series<String, Number> series = new Series<>();
-    for (Pair<String, Color> pair : player) {
+    for (Pair<String, Color> pair : players) {
       XYChart.Data<String, Number> data = new XYChart.Data<>(pair.getKey(), 0);
       dataMap.put(pair.getKey(), data);
       series.getData().add(data);
@@ -55,6 +60,23 @@ public class MoneyGraph extends BarChart<String, Number> {
 
     getData().add(series);
 
+    setBarGap(0.0);
+
+    int playerCount = players.size();
+
+    if (playerCount <= 0) {
+      setCategoryGap(0);
+    } else if (playerCount == 1) {
+      setCategoryGap(0);
+    } else {
+      double effectivePlotAreaWidth = COMPONENT_TOTAL_WIDTH - MAGIC_NUMBER;
+      double totalDesiredBarWidths = TARGET_BAR_WIDTH * playerCount;
+
+      double calculatedCategoryGap = (effectivePlotAreaWidth - totalDesiredBarWidths) / (playerCount - 1);
+
+      setCategoryGap(Math.max(0.0, calculatedCategoryGap));
+    }
+  
   }
 
   /**
